d06c371e3942e5f69046f3358b9baaba4d157a31
ultrabear <bearodark@gmail.com>
2020-01-28 10:44:10 -0800

    V0.0.1

--- team7.py
+++ team7.py
@@ -7,24 +7,14 @@
 ####
 
 team_name = 'CHLnGR'# Only 10 chars displayed
-strategy_name = 'Collude but retaliate'
-strategy_description = '''\
-Collude first round. Collude, except in a round after getting 
-a severe punishment.'''
+strategy_name = 'BETRAYAL V0.0.1'
+strategy_description = '''first beta build of the betrayal engine'''
     
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
+    '''doesnt do anything unless the enemy has betrayed'''
     if len(my_history)==0: # It's the first round; collude.
         return 'c'
-    elif my_history[-1]=='c' and their_history[-1]=='b':
+    elif 'b' in their_history:
         return 'b' # Betray if they were severely punished last time,
     else:
         return 'c' # otherwise collude.
