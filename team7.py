890094dbf3f399f1ead76de3132832daaafa9390
ultrabear <bearodark@gmail.com>
2020-01-31 10:53:25 -0800

    V 1.0.1 release beacuse trying is losing

--- team7.py
+++ team7.py
@@ -3,14 +3,14 @@
 ####
 
 team_name = 'CHLnGR'# Only 10 chars displayed
-strategy_name = 'BETRAYAL V1.0.0'
-strategy_description = '''first release of the betrayal engine'''
+strategy_name = 'BETRAYAL V1.0.1'
+strategy_description = '''second release of the betrayal engine'''
     
 def move(my_history, their_history, my_score, their_score):
-    '''doesnt do anything unless the enemy has betrayed or the enemy is winning, its a jealous engine'''
+    '''doesnt do anything unless the enemy has betrayed, nothin works better tbh'''
     if not(my_history): #its round 1, dont 
         return 'c'
-    elif 'b' in their_history or my_score <= their_score:
+    elif 'b' in their_history:
         return 'b' # finish them
     else:
         return 'c' # they havent betrayed yet
