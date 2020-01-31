028ae536b960addd3593c78762b71371087cb391
ultrabear <bearodark@gmail.com>
2020-01-31 11:45:30 -0800

    V1.0.2

--- team7.py
+++ team7.py
@@ -3,15 +3,16 @@
 ####
 
 team_name = 'CHLnGR'# Only 10 chars displayed
-strategy_name = 'BETRAYAL V1.0.1'
-strategy_description = '''second release of the betrayal engine'''
+strategy_name = 'BETRAYAL V1.0.2'
+strategy_description = '''third release of the betrayal engine'''
     
 def move(my_history, their_history, my_score, their_score):
+    import random
     '''doesnt do anything unless the enemy has betrayed, nothin works better tbh'''
-    if not(my_history): #its round 1, dont 
-        return 'c'
+    if not(my_history): #its round 1 start fast eat grass
+        return 'b'
     elif 'b' in their_history:
         return 'b' # finish them
     else:
-        return 'c' # they havent betrayed yet
+        return random.choice(["c","c","c","b"]) # they havent betrayed yet, so randomly betray
         
