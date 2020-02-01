6a315712dc4c369399123d01141cca7fb009bb44
victoriagrigoryev <60403467+victoriagrigoryev@users.noreply.github.com>
2020-01-31 16:48:02 -0800

    team 16 code and names
    added strategy and name

--- team16.py
+++ team16.py
@@ -1,3 +1,4 @@
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -6,21 +7,21 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'team0' # Only 10 chars displayed
-strategy_name = 'Collude'
-strategy_description = 'Always collude.'
+team_name = 'Team 16' # Only 10 chars displayed
+strategy_name = 'Average'
+strategy_description = 'Counts amount of betrays and colludes in the history of the other team.'
     
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
-    
-    # This player always colludes.
-    return 'c'
+    b = 0 
+    c = 0
+    for letter in their_history:
+      if letter == 'b':
+        b += 1
+    for letter in their_history:
+      if letter == 'c':
+       c += 1
+    if b > c:
+      return 'b'
+    else:
+      return 'c'
     
